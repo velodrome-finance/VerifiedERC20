03c069285dee7ae9d4c2c1c2837fe587e8fccd22
Pedro Valido <80268365+pedrovalido@users.noreply.github.com>
2025-06-02 10:25:00 +0100

    feat: IVerifiedERC20 interface

--- src/VerifiedERC20.sol
+++ src/VerifiedERC20.sol
@@ -2,19 +2,22 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-contract VerifiedERC20 is ERC20 {
+import {IVerifiedERC20} from "./interfaces/IVerifiedERC20.sol";
+
+contract VerifiedERC20 is ERC20, IVerifiedERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
 
-    function transfer(address to, uint256 value) public override returns (bool) {
+    function transfer(address to, uint256 value) public override(ERC20, IERC20) returns (bool) {
         return super.transfer({to: to, value: value});
     }
 
-    function approve(address spender, uint256 value) public override returns (bool) {
+    function approve(address spender, uint256 value) public override(ERC20, IERC20) returns (bool) {
         return super.approve({spender: spender, value: value});
     }
 
-    function transferFrom(address from, address to, uint256 value) public override returns (bool) {
+    function transferFrom(address from, address to, uint256 value) public override(ERC20, IERC20) returns (bool) {
         return super.transferFrom({from: from, to: to, value: value});
     }
 
---
+++ src/interfaces/IVerifiedERC20.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.19 <0.9.0;
+
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+interface IVerifiedERC20 is IERC20 {}
