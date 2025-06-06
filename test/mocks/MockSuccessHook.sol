da88f2ee77364d193819a75d5fdedc6c0b12ee74
Pedro Valido <80268365+pedrovalido@users.noreply.github.com>
2025-06-06 13:28:43 +0100

    chore: remove compile warnings

--- test/mocks/MockSuccessHook.sol
+++ test/mocks/MockSuccessHook.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.8.19 <0.9.0;
 import {BaseAHook} from "src/hooks/BaseAHook.sol";
 
 contract MockSuccessHook is BaseAHook {
-    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
+    function supportsInterface(bytes4) public pure override returns (bool) {
         return true;
     }
 
-    function _check(address _caller, address _address, uint256 _amount) internal override {}
+    function _check(address, address, uint256) internal override {}
 }
