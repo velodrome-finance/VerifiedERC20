6bd6a64ffbe672501022bdc4e45fd3f90aa23508
Pedro Valido <80268365+pedrovalido@users.noreply.github.com>
2025-06-09 18:08:47 +0100

    chore: update self integration according to new base hooks

--- src/hooks/extensions/SelfTransferHook.sol
+++ src/hooks/extensions/SelfTransferHook.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.19 <0.9.0;
 
-import {ITransferHook} from "../../interfaces/hooks/ITransferHook.sol";
+import {IHookRegistry} from "../../interfaces/hooks/IHookRegistry.sol";
 
-import {BaseBHook} from "../BaseBHook.sol";
+import {BaseTransferHook} from "../BaseTransferHook.sol";
 
 /**
  * @title SelfTransferHook
  * @dev Hook to restrict incentive claims to users verified on Self
  */
-contract SelfTransferHook is BaseBHook {
+contract SelfTransferHook is BaseTransferHook {
+    constructor(string memory name) BaseTransferHook(name) {}
+
+    function supportsEntrypoint(IHookRegistry.Entrypoint _entrypoint) external pure override returns (bool) {
+        return _entrypoint == IHookRegistry.Entrypoint.BEFORE_TRANSFER;
+    }
     /**
      * @dev Restrict transfers to only allow verified users to claim incentives
      * @param _caller The address of the caller
@@ -17,6 +22,7 @@ contract SelfTransferHook is BaseBHook {
      * @param _to The address of the recipient
      * @param _amount The amount being transferred
      */
+
     function _check(address _caller, address _from, address _to, uint256 _amount) internal override {
         if (_isClaimIncentive() && !_isVerified({_user: _to})) {
             revert Hook_Revert({_params: abi.encode(_caller, _from, _to, _amount)});
--- src/hooks/extensions/SinglePermissionHook.sol
+++ src/hooks/extensions/SinglePermissionHook.sol
@@ -1,29 +1,29 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.19 <0.9.0;
 
-import {IMintHook} from "../../interfaces/hooks/IMintHook.sol";
-import {IBurnHook} from "../../interfaces/hooks/IBurnHook.sol";
+import {IHookRegistry} from "../../interfaces/hooks/IHookRegistry.sol";
 
-import {BaseAHook} from "../BaseAHook.sol";
+import {BaseHook} from "../BaseHook.sol";
 
 /**
  * @title SinglePermissionHook
  * @dev Hook to restrict mint and burn to an authorized address
  */
-contract SinglePermissionHook is BaseAHook {
+contract SinglePermissionHook is BaseHook {
     /// @notice Emitted when the _authorized address passed in constructor is zero
     error SinglePermissionHook_ZeroAddress();
 
     /// @notice Address of the authorized caller
     address public immutable authorized;
 
-    constructor(address _authorized) {
+    constructor(string memory name, address _authorized) BaseHook(name) {
         if (_authorized == address(0)) revert SinglePermissionHook_ZeroAddress();
         authorized = _authorized;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IMintHook).interfaceId || interfaceId == type(IBurnHook).interfaceId;
+    function supportsEntrypoint(IHookRegistry.Entrypoint _entrypoint) external pure override returns (bool) {
+        return
+            _entrypoint == IHookRegistry.Entrypoint.BEFORE_MINT || _entrypoint == IHookRegistry.Entrypoint.BEFORE_BURN;
     }
 
     /**
@@ -32,7 +32,7 @@ contract SinglePermissionHook is BaseAHook {
      * @param _address The entity the tokens will be minted to or burned from
      * @param _amount The amount being transferred
      */
-    function _check(address _caller, address _address, uint256 _amount) internal override {
+    function _check(address _caller, address _address, uint256 _amount) view internal override {
         if (_caller != authorized) {
             revert Hook_Revert({_params: abi.encode(_caller, _address, _amount)});
         }
