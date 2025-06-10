1b38fa05ee2bd9894cfd031242f1fe1138864683
jayssj11 <cooljay.gupta@gmail.com>
2025-06-11 00:45:39 +0800

    Update SelfTransferHook.sol
    refer : https://github.com/velodrome-finance/VerifiedERC20/pull/11 ,

--- src/hooks/extensions/SelfTransferHook.sol
+++ src/hooks/extensions/SelfTransferHook.sol
@@ -1,19 +1,30 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.19 <0.9.0;
 
-import {IHookRegistry} from "../../interfaces/hooks/IHookRegistry.sol";
+import {ITransferHook} from "../../interfaces/hooks/ITransferHook.sol";
 
-import {BaseTransferHook} from "../BaseTransferHook.sol";
+import {BaseBHook} from "../BaseBHook.sol";
+
+// Interface for Self SBT contract
+interface ISelfPassportSBT {
+    function getTokenIdByAddress(address user) external view returns (uint256 tokenId);
+    function isTokenValid(uint256 tokenId) external view returns (bool valid);
+}
 
 /**
  * @title SelfTransferHook
  * @dev Hook to restrict incentive claims to users verified on Self
  */
-contract SelfTransferHook is BaseTransferHook {
-    constructor(string memory name) BaseTransferHook(name) {}
+contract SelfTransferHook is BaseBHook {
+    /// @notice The Self Passport SBT contract address
+    address public immutable selfPassportSBT;
 
-    function supportsEntrypoint(IHookRegistry.Entrypoint _entrypoint) external pure override returns (bool) {
-        return _entrypoint == IHookRegistry.Entrypoint.BEFORE_TRANSFER;
+    /**
+     * @notice Initializes the SelfTransferHook with the Self Passport SBT contract
+     * @param _selfPassportSBT The address of the Self Passport SBT contract
+     */
+    constructor(address _selfPassportSBT) {
+        selfPassportSBT = _selfPassportSBT;
     }
 
     /**
@@ -23,22 +34,38 @@ contract SelfTransferHook is BaseTransferHook {
      * @param _to The address of the recipient
      * @param _amount The amount being transferred
      */
-    function _check(address _caller, address _from, address _to, uint256 _amount) internal override {
-        if (_isClaimIncentive() && !_isVerified({_user: _to})) {
+    function _check(address _caller, address _from, address _to, uint256 _amount) internal view override {
+        if (_isClaimIncentive(_from, _to) && !_isVerified(_to)) {
             revert Hook_Revert({_params: abi.encode(_caller, _from, _to, _amount)});
         }
     }
 
     /**
      * @dev Check if the transfer is an incentive claim
+     * @param _from The sender address
+     * @param _to The recipient address
      * @return True if the transfer is a claim incentive, false otherwise
      */
-    function _isClaimIncentive() internal returns (bool) {}
+    function _isClaimIncentive(address _from, address _to) internal view returns (bool) {
+        // TODO: Implement logic to check if transfer is from incentive contract
+        // This will be implemented by the team based on their incentive contract identification strategy
+    }
 
     /**
      * @dev Check if the user is verified on Self
      * @param _user The address of the user to check
      * @return True if the user is verified, false otherwise
      */
-    function _isVerified(address _user) internal returns (bool) {}
+    function _isVerified(address _user) internal view returns (bool) {
+        // Get the token ID associated with the user
+        uint256 tokenId = ISelfPassportSBT(selfPassportSBT).getTokenIdByAddress(_user);
+        
+        // If no token ID (returns 0), user is not verified
+        if (tokenId == 0) {
+            return false;
+        }
+        
+        // Check if the token is still valid (not expired)
+        return ISelfPassportSBT(selfPassportSBT).isTokenValid(tokenId);
+    }
 }
