2dde34dfc583b20442dccf21b05830ce908b8075
Pedro Valido <80268365+pedrovalido@users.noreply.github.com>
2025-06-10 16:19:13 +0100

    fix: test setup and script

--- script/DeploySelfVerifiedERC20.s.sol
+++ script/DeploySelfVerifiedERC20.s.sol
@@ -19,22 +19,22 @@ contract DeploySelfVerifiedERC20 is Script {
         string verifiedERC20Symbol;
         address verifiedERC20Owner;
         address celo;
-        string singlePermissionHookName;
+        string singlePermissionMintHookName;
+        string singlePermissionBurnHookName;
         string selfTransferHookName;
         address verifiedERC20Factory;
         string outputFilename;
     }
 
     ERC20Lockbox public lockbox;
-    SinglePermissionHook public singlePermissionHook;
+    SinglePermissionHook public singlePermissionMintHook;
+    SinglePermissionHook public singlePermissionBurnHook;
     SelfTransferHook public selfTransferHook;
     VerifiedERC20 public verifiedERC20;
     SelfDeploymentParams internal _params;
 
     /// @dev Used by tests to disable logging of output
     bool public isTest;
-
-    function setUp() public virtual {}
 
     function run() external {
         vm.startBroadcast();
@@ -47,26 +47,23 @@ contract DeploySelfVerifiedERC20 is Script {
     }
 
     function deploy() internal virtual {
-        verifiedERC20 = VerifiedERC20(_predictedVerifiedERC20Address());
-
-        lockbox = new ERC20Lockbox({_verifiedERC20: address(verifiedERC20), _erc20: _params.celo});
-        singlePermissionHook =
-            new SinglePermissionHook({name: _params.singlePermissionHookName, _authorized: address(lockbox)});
-        selfTransferHook = new SelfTransferHook({name: _params.selfTransferHookName});
-
-        address[] memory hooks = new address[](2);
-        hooks[0] = address(singlePermissionHook);
-        hooks[1] = address(selfTransferHook);
-
         VerifiedERC20Factory verifiedERC20Factory = VerifiedERC20Factory(_params.verifiedERC20Factory);
         verifiedERC20 = VerifiedERC20(
             verifiedERC20Factory.deployVerifiedERC20({
                 _name: _params.verifiedERC20Name,
                 _symbol: _params.verifiedERC20Symbol,
                 _owner: _params.verifiedERC20Owner,
-                _hooks: hooks
+                _hooks: new address[](0) // Hook will be added to registry and activated post deployment
             })
         );
+
+        lockbox = new ERC20Lockbox({_verifiedERC20: address(verifiedERC20), _erc20: _params.celo});
+        singlePermissionMintHook =
+            new SinglePermissionHook({name: _params.singlePermissionMintHookName, _authorized: address(lockbox)});
+        singlePermissionBurnHook =
+            new SinglePermissionHook({name: _params.singlePermissionBurnHookName, _authorized: address(lockbox)});
+
+        selfTransferHook = new SelfTransferHook({name: _params.selfTransferHookName});
     }
 
     function params() external view returns (SelfDeploymentParams memory) {
@@ -75,7 +72,8 @@ contract DeploySelfVerifiedERC20 is Script {
 
     function logParams() internal view virtual {
         console.log("ERC20Lockbox: ", address(lockbox));
-        console.log("SinglePermissionHook: ", address(singlePermissionHook));
+        console.log("SinglePermissionMintHook: ", address(singlePermissionMintHook));
+        console.log("SinglePermissionBurnHook: ", address(singlePermissionBurnHook));
         console.log("SelfTransferHook: ", address(selfTransferHook));
         console.log("VerifiedERC20: ", address(verifiedERC20));
     }
@@ -85,38 +83,9 @@ contract DeploySelfVerifiedERC20 is Script {
         string memory root = vm.projectRoot();
         string memory path = string(abi.encodePacked(root, "/deployment-addresses/", _params.outputFilename));
         vm.writeJson(vm.toString(address(lockbox)), path, ".Lockbox");
-        vm.writeJson(vm.toString(address(singlePermissionHook)), path, ".SinglePermissionHook");
+        vm.writeJson(vm.toString(address(singlePermissionMintHook)), path, ".SinglePermissionMintHook");
+        vm.writeJson(vm.toString(address(singlePermissionBurnHook)), path, ".SinglePermissionBurnHook");
         vm.writeJson(vm.toString(address(selfTransferHook)), path, ".SelfTransferHook");
         vm.writeJson(vm.toString(address(verifiedERC20)), path, ".VerifiedERC20");
-    }
-
-    function _predictedVerifiedERC20Address() internal view returns (address) {
-        VerifiedERC20Factory verifiedERC20Factory = VerifiedERC20Factory(_params.verifiedERC20Factory);
-        bytes32 salt = keccak256(
-            abi.encodePacked(
-                block.chainid,
-                verifiedERC20Factory.getVerifiedERC20Count(),
-                _params.verifiedERC20Name,
-                _params.verifiedERC20Symbol,
-                _params.verifiedERC20Owner
-            )
-        );
-        bytes11 entropy = bytes11(salt);
-        salt = entropy.calculateSalt({_deployer: address(verifiedERC20Factory)});
-        bytes32 guardedSalt = keccak256(abi.encodePacked(uint256(uint160(address(verifiedERC20Factory))), salt));
-
-        address expectedVerifiedERC20 = CreateXLibrary.CREATEX.computeCreate2Address({
-            salt: guardedSalt,
-            initCodeHash: keccak256(
-                abi.encodePacked(
-                    hex"3d602d80600a3d3981f3363d3d373d3d3d363d73",
-                    bytes20(verifiedERC20Factory.implementation()),
-                    hex"5af43d82803e903d91602b57fd5bf3"
-                )
-            ),
-            deployer: address(CreateXLibrary.CREATEX)
-        });
-
-        return expectedVerifiedERC20;
     }
 }
--- test/extensions/BaseSelfForkFixture.sol
+++ test/extensions/BaseSelfForkFixture.sol
@@ -15,7 +15,8 @@ abstract contract BaseSelfForkFixture is BaseForkFixture {
     // Contracts
     TestSelfVerifiedERC20Deployment public selfVerifiedERC20Deployment;
     ERC20Lockbox public lockbox;
-    SinglePermissionHook public singlePermissionHook;
+    SinglePermissionHook public singlePermissionMintHook;
+    SinglePermissionHook public singlePermissionBurnHook;
     SelfTransferHook public selfTransferHook;
     DeploySelfVerifiedERC20.SelfDeploymentParams internal _selfParams;
 
@@ -24,6 +25,8 @@ abstract contract BaseSelfForkFixture is BaseForkFixture {
         createUsers();
 
         deployContracts();
+
+        postDeployment();
         labelContracts();
     }
 
@@ -35,25 +38,52 @@ abstract contract BaseSelfForkFixture is BaseForkFixture {
             verifiedERC20Symbol: "VerifiedCelo",
             verifiedERC20Owner: users.owner,
             celo: CELO,
-            singlePermissionHookName: "Single Permission Hook to restrict mints and burns to the lockbox",
-            selfTransferHookName: "Self Transfer Hook to restrict incentive claims to verified users",
+            singlePermissionMintHookName: "Single Permission Hook to restrict mints to the lockbox",
+            singlePermissionBurnHookName: "Single Permission Hook to restrict burns to the lockbox",
+            selfTransferHookName: "Self Transfer Hook to restrict incentive claims to users verified on self",
             verifiedERC20Factory: address(verifiedERC20Factory),
             outputFilename: ""
         });
-        selfVerifiedERC20Deployment = new TestSelfVerifiedERC20Deployment({_params: _selfParams});
+        selfVerifiedERC20Deployment = new TestSelfVerifiedERC20Deployment({_selfParams: _selfParams});
         selfVerifiedERC20Deployment.run();
 
         lockbox = selfVerifiedERC20Deployment.lockbox();
-        singlePermissionHook = selfVerifiedERC20Deployment.singlePermissionHook();
+        singlePermissionMintHook = selfVerifiedERC20Deployment.singlePermissionMintHook();
+        singlePermissionBurnHook = selfVerifiedERC20Deployment.singlePermissionBurnHook();
         selfTransferHook = selfVerifiedERC20Deployment.selfTransferHook();
         verifiedERC20 = selfVerifiedERC20Deployment.verifiedERC20();
+    }
+
+    function postDeployment() internal {
+        // Register hooks in hook registry
+        vm.startPrank(users.owner);
+        hookRegistry.registerHook({
+            _hook: address(singlePermissionMintHook),
+            _entrypoint: IHookRegistry.Entrypoint.BEFORE_MINT
+        });
+        hookRegistry.registerHook({
+            _hook: address(singlePermissionBurnHook),
+            _entrypoint: IHookRegistry.Entrypoint.BEFORE_BURN
+        });
+        hookRegistry.registerHook({
+            _hook: address(selfTransferHook),
+            _entrypoint: IHookRegistry.Entrypoint.BEFORE_TRANSFER
+        });
+        vm.stopPrank();
+
+        // Activate hooks in verified ERC20
+        vm.startPrank(users.owner);
+        verifiedERC20.activateHook({_hook: address(singlePermissionMintHook)});
+        verifiedERC20.activateHook({_hook: address(singlePermissionBurnHook)});
+        verifiedERC20.activateHook({_hook: address(selfTransferHook)});
     }
 
     function labelContracts() internal override {
         super.labelContracts();
 
         vm.label({account: address(selfVerifiedERC20Deployment), newLabel: "SelfVerifiedERC20Deployment"});
-        vm.label({account: address(singlePermissionHook), newLabel: "SinglePermissionHook"});
+        vm.label({account: address(singlePermissionMintHook), newLabel: "SinglePermissionMintHook"});
+        vm.label({account: address(singlePermissionBurnHook), newLabel: "SinglePermissionBurnHook"});
         vm.label({account: address(selfTransferHook), newLabel: "SelfTransferHook"});
     }
 }
