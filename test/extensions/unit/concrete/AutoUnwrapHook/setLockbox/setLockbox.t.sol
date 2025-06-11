edc3d4ada930a7fbb579e1f5a8e37e5207a5eb9a
Pedro <80268365+pedrovalido@users.noreply.github.com>
2025-06-11 20:48:34 +0100

    feat: auto unwrap hook on incentive claim (#15)

---
+++ test/extensions/unit/concrete/AutoUnwrapHook/setLockbox/setLockbox.t.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.19 <0.9.0;
+
+import "../AutoUnwrapHook.t.sol";
+
+contract SetLockboxConcreteTest is AutoUnwrapHookTest {
+    function test_WhenTheLockboxPassedIsTheZeroAddress() external {
+        // It should revert with {AutoUnwrapHook_ZeroAddress}
+        address _lockbox = address(0);
+        vm.expectRevert(abi.encodeWithSelector(AutoUnwrapHook.AutoUnwrapHook_ZeroAddress.selector));
+        autoUnwrapHook.setLockbox({_verifiedERC20: address(verifiedERC20), _lockbox: _lockbox});
+    }
+
+    modifier whenTheLockboxPassedIsNotTheZeroAddress() {
+        _;
+    }
+
+    function test_WhenTheVerifiedERC20PassedIsTheZeroAddress() external whenTheLockboxPassedIsNotTheZeroAddress {
+        // It should revert with {AutoUnwrapHook_ZeroAddress}
+        address _lockbox = address(lockbox);
+        address _verifiedERC20 = address(0);
+        vm.expectRevert(abi.encodeWithSelector(AutoUnwrapHook.AutoUnwrapHook_ZeroAddress.selector));
+        autoUnwrapHook.setLockbox({_verifiedERC20: _verifiedERC20, _lockbox: _lockbox});
+    }
+
+    modifier whenTheVerifiedERC20PassedIsNotTheZeroAddress() {
+        _;
+    }
+
+    function test_WhenTheCallerIsNotTheVerifiedERC20Owner()
+        external
+        whenTheLockboxPassedIsNotTheZeroAddress
+        whenTheVerifiedERC20PassedIsNotTheZeroAddress
+    {
+        // It should revert with {AutoUnwrapHook_NotAuthorized}
+        address _lockbox = address(lockbox);
+        address _verifiedERC20 = address(verifiedERC20);
+        vm.startPrank(users.charlie);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AutoUnwrapHook.AutoUnwrapHook_NotAuthorized.selector, users.charlie, _verifiedERC20, _lockbox
+            )
+        );
+        autoUnwrapHook.setLockbox({_verifiedERC20: _verifiedERC20, _lockbox: _lockbox});
+    }
+
+    function test_WhenTheCallerIsTheVerifiedERC20Owner()
+        external
+        whenTheLockboxPassedIsNotTheZeroAddress
+        whenTheVerifiedERC20PassedIsNotTheZeroAddress
+    {
+        // It should set the lockbox mapping
+        // It should emit a {LockboxSet} event
+        address _lockbox = users.alice;
+        address _verifiedERC20 = address(verifiedERC20);
+
+        vm.startPrank(users.owner);
+        vm.expectEmit(address(autoUnwrapHook));
+        emit AutoUnwrapHook.LockboxSet({verifiedERC20: _verifiedERC20, lockbox: _lockbox});
+        autoUnwrapHook.setLockbox({_verifiedERC20: _verifiedERC20, _lockbox: _lockbox});
+
+        assertEq(autoUnwrapHook.lockbox({_verifiedERC20: _verifiedERC20}), _lockbox);
+    }
+}
