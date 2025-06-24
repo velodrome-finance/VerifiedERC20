4a81405b0d580a7e7a0fc47e0b1e73817a17334d
Pedro Valido <80268365+pedrovalido@users.noreply.github.com>
2025-06-24 09:10:46 +0100

    nit: constant for user balance

--- test/extensions/unit/concrete/VerifiedERC20/burn/burn.t.sol
+++ test/extensions/unit/concrete/VerifiedERC20/burn/burn.t.sol
@@ -4,14 +4,15 @@ pragma solidity >=0.8.19 <0.9.0;
 import "../VerifiedERC20.t.sol";
 
 contract BurnConcreteTest is VerifiedERC20Test {
+  uint256 userBalance = 1000;
     function setUp() public override {
         super.setUp();
 
         vm.prank(address(lockbox));
-        verifiedERC20.mint({_account: users.alice, _value: 1000});
+        verifiedERC20.mint({_account: users.alice, _value: userBalance});
 
         vm.prank(address(lockbox));
-        verifiedERC20.mint({_account: address(lockbox), _value: 1000});
+        verifiedERC20.mint({_account: address(lockbox), _value: userBalance});
     }
 
     modifier whenTheCallerIsTheAccount() {
@@ -20,7 +21,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
 
     function test_WhenTheCallerIsNotLockbox() external whenTheCallerIsTheAccount {
         // It should revert with {VerifiedERC20_HookRevert}
-        uint256 _amount = 1000;
+        uint256 _amount = userBalance;
         address _account = users.alice;
         address _caller = _account;
         vm.expectRevert(
@@ -46,10 +47,10 @@ contract BurnConcreteTest is VerifiedERC20Test {
         whenTheCallerIsTheLockbox
     {
         // It should revert with {ERC20InsufficientBalance}
-        uint256 _amount = 1000 + 1;
+        uint256 _amount = userBalance + 1;
         address _account = address(lockbox);
 
-        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, _account, 1000, _amount));
+        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, _account, userBalance, _amount));
         verifiedERC20.burn({_account: _account, _value: _amount});
     }
 
@@ -61,7 +62,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
         // It should call the single permission burn hook
         // It should emit a {Transfer} event
         // It should burn the amount from the user
-        uint256 _amount = 1000 - 1;
+        uint256 _amount = userBalance - 1;
         address _account = address(lockbox);
 
         vm.expectCall({
@@ -73,7 +74,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
         emit IERC20.Transfer({from: _account, to: address(0), value: _amount});
         verifiedERC20.burn({_account: _account, _value: _amount});
 
-        assertEq(verifiedERC20.balanceOf({account: _account}), 1000 - _amount);
+        assertEq(verifiedERC20.balanceOf({account: _account}), userBalance - _amount);
     }
 
     modifier whenTheCallerIsNotTheAccount() {
@@ -82,7 +83,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
 
     function test_WhenTheAmountIsGreaterThanTheAllowance() external whenTheCallerIsNotTheAccount {
         // It should revert with {ERC20InsufficientAllowance}
-        uint256 _amount = 1000;
+        uint256 _amount = userBalance;
         address _account = users.alice;
         address _caller = users.charlie;
 
@@ -95,7 +96,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
 
     modifier whenTheAmountIsSmallerOrEqualToTheAllowance(address _caller) {
         vm.prank(users.alice);
-        verifiedERC20.approve({spender: _caller, value: 1000 + 1});
+        verifiedERC20.approve({spender: _caller, value: userBalance + 1});
         _;
     }
 
@@ -105,7 +106,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
         whenTheAmountIsSmallerOrEqualToTheAllowance(users.charlie)
     {
         // It should revert with {VerifiedERC20_HookRevert}
-        uint256 _amount = 1000;
+        uint256 _amount = userBalance;
         address _account = users.alice;
         address _caller = users.charlie;
         vm.expectRevert(
@@ -132,10 +133,10 @@ contract BurnConcreteTest is VerifiedERC20Test {
         whenTheCallerIsTheLockbox_
     {
         // It should revert with {ERC20InsufficientBalance}
-        uint256 _amount = 1000 + 1;
+        uint256 _amount = userBalance + 1;
         address _account = users.alice;
 
-        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, _account, 1000, _amount));
+        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, _account, userBalance, _amount));
         verifiedERC20.burn({_account: _account, _value: _amount});
     }
 
@@ -149,7 +150,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
         // It should deduct the allowance
         // It should emit a {Transfer} event
         // It should burn the amount from the user
-        uint256 _amount = 1000 - 1;
+        uint256 _amount = userBalance - 1;
         address _account = users.alice;
 
         vm.expectCall({
@@ -161,8 +162,8 @@ contract BurnConcreteTest is VerifiedERC20Test {
         emit IERC20.Transfer({from: _account, to: address(0), value: _amount});
         verifiedERC20.burn({_account: _account, _value: _amount});
 
-        assertEq(verifiedERC20.balanceOf({account: _account}), 1000 - _amount);
-        assertEq(verifiedERC20.allowance({spender: address(lockbox), owner: _account}), 1000 + 1 - _amount);
+        assertEq(verifiedERC20.balanceOf({account: _account}), userBalance - _amount);
+        assertEq(verifiedERC20.allowance({spender: address(lockbox), owner: _account}), userBalance + 1 - _amount);
     }
 
     function testGas_burn()
@@ -171,7 +172,7 @@ contract BurnConcreteTest is VerifiedERC20Test {
         whenTheAmountIsSmallerOrEqualToTheAllowance(address(lockbox))
         whenTheCallerIsTheLockbox_
     {
-        uint256 _amount = 1000 - 1;
+        uint256 _amount = userBalance - 1;
         address _account = users.alice;
 
         verifiedERC20.burn({_account: _account, _value: _amount});
