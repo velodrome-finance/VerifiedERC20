bf3bb77186d59d9756b8d24a631a0d6e6040b6ab
Pedro <80268365+pedrovalido@users.noreply.github.com>
2025-08-15 14:20:16 +0100

    chore: deploy celo self (#29)
    * feat: verified erc20 celo
    
    * chore: deterministic verifierc20 deployment addresses
    
    * feat: self verified erc20 celo script
    
    * chore: deploy self.xyx celo

---
+++ script/deploySelfVerifiedERC20/DeploySelfVerifiedERC20Celo.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {DeploySelfVerifiedERC20} from "../DeploySelfVerifiedERC20.s.sol";
+
+contract DeploySelfVerifiedERC20Celo is DeploySelfVerifiedERC20 {
+    function setUp() public override {
+        _params = DeploySelfVerifiedERC20.SelfDeploymentParams({
+            verifiedERC20Name: "Self.xyz Verified Celo",
+            verifiedERC20Symbol: "vCELO",
+            verifiedERC20Owner: 0xbf4d9C534c1a8B58a9AdC4a015c4432c744551D2,
+            celo: 0x471EcE3750Da237f93B8E339c536989b8978a438,
+            singlePermissionMintHookName: "Single Permission Hook to restrict mints to the lockbox",
+            singlePermissionBurnHookName: "Single Permission Hook to restrict burns to the lockbox",
+            selfTransferHookName: "Hook to restrict incentive claims only to Self.xyz verified wallets",
+            voter: 0x97cDBCe21B6fd0585d29E539B1B99dAd328a1123, //leaf voter on celo
+            selfPassportSBT: 0x2B734685ad181c003A5d1BbC3E683DAb4f50b3F6,
+            autoUnwrapHookName: "Hook for incentive claims to automatically unwrap to the base token",
+            verifiedERC20Factory: 0x270176c42dAaFF1CEb6369601Ebe1d2e9Bb9218F,
+            outputFilename: "celo-self.json"
+        });
+    }
+}
